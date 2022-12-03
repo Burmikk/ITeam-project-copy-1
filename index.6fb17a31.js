66f1ace9d1151089d8709bc83b81d63d08c3806e
Burmikk <116118020+Burmikk@users.noreply.github.com>
2022-12-03 12:19:21 +0000

    Deploying to gh-pages from @ Burmikk/ITeam-project-copy-1@9173dedee60ebe943fab21ee794967e9a8fce56e 🚀

---
+++ index.6fb17a31.js
@@ -0,0 +1,2 @@
+function changeHeaderBackground(){var e=document.querySelector("[data-header]"),a=e.offsetHeight;window.pageYOffset>a?e.classList.add("header--transparency"):e.classList.remove("header--transparency")}window.onscroll=function(){return changeHeaderBackground()};
+//# sourceMappingURL=index.6fb17a31.js.map
